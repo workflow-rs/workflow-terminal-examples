9ff39db9e9da47de81347ea262b925a2395d6318
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2022-09-16 18:06:17 +0300

    error print

--- native/src/main.rs
+++ native/src/main.rs
@@ -1,8 +1,9 @@
-use workflow_terminal::Result;
 use cli::example_terminal;
 
 #[async_std::main]
-async fn main() -> Result<()>{
-    example_terminal().await?;
-    Ok(())
+async fn main() {
+    let result = example_terminal().await;
+    if let Err(err) = result {
+        println!("{}", err);
+    }
 }
