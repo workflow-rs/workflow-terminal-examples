0b202cc0256d5d5cbc480782c7525cf44a2daee4
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2022-09-16 14:39:34 +0300

    resolving pipe() ordering issue

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,4 +1,3 @@
-// use workflow_terminal::{Result, cli::Terminal as TerminalTrait};
 use std::sync::{Arc,Mutex};
 use async_trait::async_trait;
 use workflow_terminal::Terminal;
@@ -21,22 +20,30 @@ impl ExampleCli {
         }
     }
 
-    fn term(&self) -> Arc<Terminal> {
-        self.term.lock().unwrap().as_ref().unwrap().clone()
+    fn term(&self) -> Option<Arc<Terminal>> {
+        if let Some(term) = self.term.lock().unwrap().as_ref() { //; //.unwrap().clone()
+            Some(term.clone())
+        } else {
+            None
+        }
     }
 
 }
 
 impl workflow_log::Sink for ExampleCli {
     fn write(&self, _level:Level, args : &std::fmt::Arguments<'_>) -> bool {
-        
-        self.term().writeln(args.to_string());
-        // self.term().writeln("HELLO WORLD".to_string());
-        // self.term().writeln(args);
-        // return: 
-        // - false for default log output handling (print to stdout or web console)
-        // - true, to disable further processing (no further output is made)
-        true
+
+        // note, the terminal may not be initialized
+        // if workflow_log::pipe() is bound before the
+        // Terminal::init() is complete.
+        if let Some(term) = self.term() {
+            term.writeln(args.to_string());
+            // true to disable further processing (no further output is made)
+            true
+        } else {
+            // false for default log output handling (print to stdout or web console)
+            false
+        }
     }
 }
 
@@ -49,10 +56,11 @@ impl Cli for ExampleCli {
     }
 
     async fn digest(&self, term : &Arc<Terminal>, cmd: String) -> Result<()> {
-        //println!("cmd:: {}", cmd);
         let argv = parse(&cmd);
-        //println!("argv[0]:: {}", argv[0]);
         match argv[0].as_str() {
+            "help" => {
+                term.writeln("\n\rCommands:\n\r\thelp - this list\n\r\thello - simple terminal text output\n\r\ttest - workflow_log!() macro test\n\r\texit - exits example (native only)\n\r");
+            },
             "hello" => {
                 term.writeln("hello back to you!");
             },
@@ -71,8 +79,11 @@ impl Cli for ExampleCli {
         Ok(())
     }
 
-    async fn complete(&self, _term : &Arc<Terminal>, substring : String) -> Result<Vec<String>> {
-        if substring.starts_with('a') {
+    async fn complete(&self, _term : &Arc<Terminal>, cmd : String) -> Result<Vec<String>> {
+        let argv = parse(&cmd);
+        if argv.len() == 0 { return Ok(vec![]) }
+        let last = argv.last().unwrap();
+        if last.starts_with('a') {
             Ok(vec!["alpha".to_string(), "aloha".to_string(), "albatross".to_string()])
         } else {
             Ok(vec![])
@@ -83,12 +94,15 @@ impl Cli for ExampleCli {
 pub async fn example_terminal() -> Result<()> {
 
     let cli = Arc::new(ExampleCli::new());
+    let term = Arc::new(Terminal::try_new(cli.clone(),"$ ")?);
+    term.init().await?;
 
+    // IMPORTANT: if redirecting workflow_log, using pipe()
+    // the handler must be installed after Terminal::init()
+    // is invoked.
     workflow_log::pipe(Some(cli.clone()));
 
-    let term = Arc::new(Terminal::try_new(cli,"$ ")?);
-    term.init().await?;
-    term.writeln("Terminal example");
+    term.writeln("Terminal example (type 'help' for list of commands)");
     term.run().await?;
 
     Ok(())
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -4,6 +4,9 @@ use cli::example_terminal;
 
 #[wasm_bindgen(start)]
 pub async fn load_terminal() -> Result<()>{
+
+    console_error_panic_hook::set_once();
+
     example_terminal().await?;
     Ok(())
 }
